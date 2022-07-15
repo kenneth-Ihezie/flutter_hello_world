9a9d327654e06027a97b6f5178aed532066dec4b
collins ihezie <collinsihezie6@gmail.com>
2022-07-15 08:46:37 +0100

    Added biz card view

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:hello_word/views/biz_card.dart';
 import 'package:hello_word/views/home.dart';
 import 'package:hello_word/views/scaffold_example.dart';
 
 void main() {
   //we are just changing classes
   runApp(const MaterialApp(
-    home: ScaffoldExample(),
+    home: BizCard(),
   ));
 }
---
+++ lib/views/biz_card.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+class BizCard extends StatelessWidget {
+  const BizCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("BizCard"),
+      ),
+      body: Container(
+        alignment: Alignment.center,
+        child: Stack(
+          alignment: Alignment.bottomCenter,
+          children:  [
+            _getCard(),
+            _getAvatar()
+          ],
+        ),
+      ),
+    );
+  }
+
+  Container _getCard(){
+    return Container(
+      width: 350,
+      height: 200,
+      margin: EdgeInsets.all(50.0),
+      decoration: BoxDecoration(
+        color: Colors.pinkAccent,
+        borderRadius: BorderRadius.circular(14.5)
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text("Collins Ihezie", style: TextStyle(
+            fontSize: 20.9,
+            fontWeight: FontWeight.w500,
+            color: Colors.white
+          ),),
+          const Text("collinsihezie6@gmail.com"),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              Icon(Icons.person),
+              Text("T: @starrex_dev")
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  Container _getAvatar(){
+    return Container(
+      width: 100,
+      height: 100,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: const BorderRadius.all(Radius.circular(50.0)),
+        border: Border.all(color: Colors.redAccent, width: 1.2),
+        image: const DecorationImage(image: NetworkImage("https://picsum.photos/300/300"))
+      ),
+    );
+  }
+}
